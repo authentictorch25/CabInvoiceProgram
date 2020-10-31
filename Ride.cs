4ad1f0ab6d28a37923d858811885d99ce86a1812
authentictorch25 <64377971+authentictorch25@users.noreply.github.com>
2020-11-01 00:47:49 +0530

    [Akash]Added UC2 program for multiple ride fare calcualtion

---
+++ Ride.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGenerator
+{
+    /// <summary>
+    /// Ride class to get ride based on the travel time and distance
+    /// </summary
+    public class Ride
+    {
+        public double distance;
+        public int minutes;
+
+        public Ride(double distance, int minutes)
+        {
+            this.distance = distance;
+            this.minutes = minutes;
+        }
+    }
+}
