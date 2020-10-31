7d15306cdc9fda3a4c9198f7257a67548a78f09a
authentictorch25 <64377971+authentictorch25@users.noreply.github.com>
2020-11-01 00:31:53 +0530

    [Akash]Added CalculateFare method

---
+++ CabInvoiceCustomException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGenerator
+{
+    public class CabInvoiceCustomException : Exception
+    {
+        // <summary>
+        /// Enum class Exception Type
+        /// </summary>
+        public enum ExceptionType
+        {
+            INVALID_DISTANCE,
+            INVALID_TIME
+        }
+
+        public ExceptionType exceptionType;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CabInvoiceCustomException"/> class.
+        /// </summary>
+        /// <param name="exception">The exception.</param>
+        /// <param name="message">The message.</param>
+        public CabInvoiceCustomException(ExceptionType exception, string message) : base(message)
+        {
+            this.exceptionType = exception;
+        }
+    }
+}
